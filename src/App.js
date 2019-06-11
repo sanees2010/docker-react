cee32d76d260f578df3deb8ad171a6a2ea599572
root <root@ip-172-31-31-125.ec2.internal>
2019-06-11 20:31:00 +0000

    faisal 6

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hello how are you</code> and save to reload.
+          hello <code> how are you</code> and save to reload.
         </p>
         <a
           className="App-link"
